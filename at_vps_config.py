3de28ce3d55dcf3c03dafb256d2f63a48eeaaefe
maglore9900 <maglore46@gmail.com>
2022-11-26 11:03:52 -0500

    Update at_vps_config.py

--- at_vps_config.py
+++ at_vps_config.py
@@ -21,9 +21,11 @@ print(f"We are now updating your server, ensuring it has the latest patches, and
 os.system(f'apt update && apt autoremove --purge ubuntu-desktop && apt -y install gnome-session gnome-terminal && apt -y install xfe && apt -y install lxterminal && apt -y install htop')
 
 print(f"Installing nomachine")
-os.system(f'apt update && apt -y install wget && wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb && apt install -f  ./nomachine_7.10.1_1_amd64.deb && systemctl start nxserver.service')
+# os.system(f'apt update && apt -y install wget && wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb && apt install -f  ./nomachine_7.10.1_1_amd64.deb && systemctl start nxserver.service')
+os.system(f'wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb && apt install -f  ./nomachine_7.10.1_1_amd64.deb && systemctl start nxserver.service')
 
 print(f"Installing brave browser")
 os.system(f'apt -y install apt-transport-https curl && curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && apt update && apt -y install brave-browser')
 
 print(f"Configuration complete, rebooting your computer when you hit the enter key, remember, when you log in next, use your new account {user}")
+input()
